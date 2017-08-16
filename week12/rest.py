b0aed6c69ce25b42a8732d1bec1f4f7804d341a7
Edward Fine <edward.fine@gmail.com>
2017-08-15 20:11:41 -0700

    Update weeks 12 and 13

--- week12/rest.py
+++ week12/rest.py
@@ -14,18 +14,18 @@ app = web.application(urls, globals())
 
 class list_users:        
     def GET(self):
-	output = 'users:[';
-	for child in root:
-                print 'child', child.tag, child.attrib
-                output += str(child.attrib) + ','
-	output += ']';
+        output = 'users:[';
+        for child in root:
+            print('child', child.tag, child.attrib)
+            output += str(child.attrib) + ','
+        output += ']';
         return output
 
 class get_user:
     def GET(self, user):
-	for child in root:
-		if child.attrib['id'] == user:
-		    return str(child.attrib)
+        for child in root:
+            if child.attrib['id'] == user:
+                return str(child.attrib)
 
 if __name__ == "__main__":
     app.run()
